f36720a96358e986e6827da99d05eb65ba30636a
DNKpp <DNKpp2011@gmail.com>
2020-08-11 12:49:53 +0200

    modernize arithmetic and bitwise operators
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Utility/operators/arithmetic.hpp
+++ include/Simple-Utility/operators/arithmetic.hpp
@@ -9,329 +9,73 @@
 
 #pragma once
 
-namespace sl {
-namespace operators {
+#include "Simple-Utility/concepts/arithmetic.hpp"
 
-	/*!
-	 * \struct	Plus
-	 *
-	 * \brief	Single template helper struct for additions. Provides the inheritor with an implementation of operator +(const T&, const T&) as friend function.
-	 * 			The inheritors must implement the operator +=(const T&, const T&) themselves.
-	 *
-	 * \tparam	T	Generic type parameter.
-	 */
-	template <class T>
+namespace sl::operators
+{
 	struct Plus
 	{
-		/*!
-		 * \brief	Addition operator.
-		 *
-		 * \param	_lhs	The first value.
-		 * \param	_rhs	A value to add to it.
-		 *
-		 * \return	The result of the operation.
-		 */
-		friend constexpr T operator +(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::AddAssignable<T1, T2>
+		friend constexpr T1 operator +(T1 lhs, const T2& rhs) noexcept(noexcept(lhs += rhs))
 		{
-			T tmp(_lhs);
-			tmp += _rhs;
-			return tmp;
+			lhs += rhs;
+			return lhs;
 		}
 	};
 
-	/*!
-	* \struct	Minus
-	*
-	* \brief	Single template helper struct for subtractions. Provides the inheritor with an implementation of operator -(const T&, const T&) as friend function.
-	* 			The inheritors must implement the operator -=(const T&, const T&) themselves.
-	*
-	* \tparam	T	Generic type parameter.
-	*/
-	template <class T>
 	struct Minus
 	{
-		/*!
-		 * \brief	Subtraction operator.
-		 *
-		 * \param	_lhs	The first value.
-		 * \param	_rhs	A value to subtract from it.
-		 *
-		 * \return	The result of the operation.
-		 */
-		friend constexpr T operator -(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::SubtractAssignable<T1, T2>
+		friend constexpr T1 operator -(T1 lhs, const T2& rhs) noexcept(noexcept(lhs -= rhs))
 		{
-			T tmp(_lhs);
-			tmp -= _rhs;
-			return tmp;
+			lhs -= rhs;
+			return lhs;
 		}
 	};
 
-	/*!
-	* \struct	Multiply
-	*
-	* \brief	Single template helper struct for multiplications. Provides the inheritor with an implementation of operator *(const T&, const T&) as friend function.
-	* 			The inheritors must implement the operator *=(const T&, const T&) themselves.
-	*
-	* \tparam	T	Generic type parameter.
-	*/
-	template <class T>
 	struct Multiply
 	{
-		/*!
-		 * \brief	Multiplication operator.
-		 *
-		 * \param	_lhs	The first value to multiply.
-		 * \param	_rhs	The second value to multiply.
-		 *
-		 * \return	The result of the operation.
-		 */
-		friend constexpr T operator *(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::MultiplyAssignable<T1, T2>
+		friend constexpr T1 operator *(T1 lhs, const T2& rhs) noexcept(noexcept(lhs *= rhs))
 		{
-			T tmp(_lhs);
-			tmp *= _rhs;
-			return tmp;
+			lhs *= rhs;
+			return lhs;
 		}
 	};
 
-	/*!
-	* \struct	Divide
-	*
-	* \brief	Single template helper struct for divisions. Provides the inheritor with an implementation of operator /(const T&, const T&) as friend function.
-	* 			The inheritors must implement the operator /=(const T&, const T&) themselves.
-	*
-	* \tparam	T	Generic type parameter.
-	*/
-	template <class T>
 	struct Divide
 	{
-		/*!
-		 * \brief	Division operator.
-		 *
-		 * \param	_lhs	The numerator.
-		 * \param	_rhs	The denominator.
-		 *
-		 * \return	The result of the operation.
-		 */
-		friend constexpr T operator /(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::DivideAssignable<T1, T2>
+		friend constexpr T1 operator /(T1 lhs, const T2& rhs) noexcept(noexcept(lhs /= rhs))
 		{
-			T tmp(_lhs);
-			tmp /= _rhs;
-			return tmp;
+			lhs /= rhs;
+			return lhs;
 		}
 	};
 
-	/*!
-	* \struct	Modulo
-	*
-	* \brief	Single template helper struct for modulo. Provides the inheritor with an implementation of operator %(const T&, const T&) as friend function.
-	* 			The inheritors must implement the operator %=(const T&, const T&) themselves.
-	*
-	* \tparam	T	Generic type parameter.
-	*/
-	template <class T>
 	struct Modulo
 	{
-		/*!
-		 * \brief	Modulus operator.
-		 *
-		 * \param	_lhs	The numerator.
-		 * \param	_rhs	The denominator.
-		 *
-		 * \return	The result of the operation.
-		 */
-		friend constexpr T operator %(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::ModuloAssignable<T1, T2>
+		friend constexpr T1 operator %(T1 lhs, const T2& rhs) noexcept(noexcept(lhs %= rhs))
 		{
-			T tmp(_lhs);
-			tmp %= _rhs;
-			return tmp;
+			lhs %= rhs;
+			return lhs;
 		}
 	};
 
-	/*!
-	* \struct	Arithmetic
-	*
-	* \brief	Single template helper struct for all arithmetic operations. Look at the inherited classes for more details.
-	*
-	* \tparam	T	Generic type parameter.
-	*/
-	template <class T>
 	struct Arithmetic :
-		Plus<T>,
-		Minus<T>,
-		Multiply<T>,
-		Divide<T>,
-		Modulo<T>
-	{
-	};
-
-	/*!
-	* \struct	Plus2
-	*
-	* \brief	Multi template helper struct for additions. Provides the inheritor with an implementation of operator +(const T1&, const T2&) as friend function.
-	* 			The inheritors must implement the operator +=(const T1&, const T2&) themselves.
-	* \remark	Because lack of consistency, the reversed operator +=(const T2&, const T1&) isn't implemented by this struct. If you need it, you either have to provide it by yourself
-	* 			or inherit Plus2<T2, T1>.
-	*
-	* \tparam	T1	Generic type parameter.
-	* \tparam	T2	Generic type parameter.
-	*/
-	template <class T1, class T2>
-	struct Plus2
-	{
-		/*!
-		* \brief	Addition operator.
-		*
-		* \param	_lhs	The first value.
-		* \param	_rhs	A value to add to it.
-		*
-		* \return	The result of the operation.
-		*/
-		friend constexpr T1 operator +(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp += _rhs;
-			return tmp;
-		}
-	};
-
-	/*!
-	* \struct	Minus2
-	*
-	* \brief	Multi template helper struct for subtractions. Provides the inheritor with an implementation of operator -(const T1&, const T2&) as friend function.
-	* 			The inheritors must implement the operator -=(const T1&, const T2&) themselves.
-	* \remark	Because lack of consistency, the reversed operator -=(const T2&, const T1&) isn't implemented by this struct. If you need it, you either have to provide it by yourself
-	* 			or inherit Minus2<T2, T1>.
-	*
-	* \tparam	T1	Generic type parameter.
-	* \tparam	T2	Generic type parameter.
-	*/
-	template <class T1, class T2>
-	struct Minus2
-	{
-		/*!
-		* \brief	Subtraction operator.
-		*
-		* \param	_lhs	The first value.
-		* \param	_rhs	A value to subtract from it.
-		*
-		* \return	The result of the operation.
-		*/
-		friend constexpr T1 operator -(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp -= _rhs;
-			return tmp;
-		}
-	};
-
-	/*!
-	* \struct	Multiply2
-	*
-	* \brief	Multi template helper struct for multiplications. Provides the inheritor with an implementation of operator *(const T1&, const T2&) as friend function.
-	* 			The inheritors must implement the operator *=(const T1&, const T2&) themselves.
-	* \remark	Because lack of consistency, the reversed operator *=(const T2&, const T1&) isn't implemented by this struct. If you need it, you either have to provide it by yourself
-	* 			or inherit Multiply2<T2, T1>.
-	*
-	* \tparam	T1	Generic type parameter.
-	* \tparam	T2	Generic type parameter.
-	*/
-	template <class T1, class T2>
-	struct Multiply2
-	{
-		/*!
-		* \brief	Multiplication operator.
-		*
-		* \param	_lhs	The first value to multiply.
-		* \param	_rhs	The second value to multiply.
-		*
-		* \return	The result of the operation.
-		*/
-		friend constexpr T1 operator *(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp *= _rhs;
-			return tmp;
-		}
-	};
-
-	/*!
-	* \struct	Divide2
-	*
-	* \brief	Multi template helper struct for divisions. Provides the inheritor with an implementation of operator /(const T1&, const T2&) as friend function.
-	* 			The inheritors must implement the operator /=(const T1&, const T2&) themselves.
-	* \remark	Because lack of consistency, the reversed operator /=(const T2&, const T1&) isn't implemented by this struct. If you need it, you either have to provide it by yourself
-	* 			or inherit Divide2<T2, T1>.
-	*
-	* \tparam	T1	Generic type parameter.
-	* \tparam	T2	Generic type parameter.
-	*/
-	template <class T1, class T2>
-	struct Divide2
-	{
-		/*!
-		* \brief	Division operator.
-		*
-		* \param	_lhs	The numerator.
-		* \param	_rhs	The denominator.
-		*
-		* \return	The result of the operation.
-		*/
-		friend constexpr T1 operator /(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp /= _rhs;
-			return tmp;
-		}
-	};
-
-	/*!
-	* \struct	Modulo2
-	*
-	* \brief	Multi template helper struct for modulo. Provides the inheritor with an implementation of operator %(const T1&, const T2&) as friend function.
-	* 			The inheritors must implement the operator %=(const T1&, const T2&) themselves.
-	* \remark	Because lack of consistency, the reversed operator %=(const T2&, const T1&) isn't implemented by this struct. If you need it, you either have to provide it by yourself
-	* 			or inherit Modulo2<T2, T1>.
-	*
-	* \tparam	T1	Generic type parameter.
-	* \tparam	T2	Generic type parameter.
-	*/
-	template <class T1, class T2>
-	struct Modulo2
-	{
-		/*!
-		* \brief	Modulus operator.
-		*
-		* \param	_lhs	The numerator.
-		* \param	_rhs	The denominator.
-		*
-		* \return	The result of the operation.
-		*/
-		friend constexpr T1 operator %(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp %= _rhs;
-			return tmp;
-		}
-	};
-
-	/*!
-	* \struct	Arithmetic2
-	*
-	* \brief	Multi template helper struct for all arithmetic operations. Look at the inherited classes for more details.
-	*
-	* \tparam	T1	Generic type parameter.
-	* \tparam	T2	Generic type parameter.
-	*/
-	template <class T1, class T2>
-	struct Arithmetic2 :
-		Plus2<T1, T2>,
-		Minus2<T1, T2>,
-		Multiply2<T1, T2>,
-		Divide2<T1, T2>,
-		Modulo2<T1, T2>
+		Plus,
+		Minus,
+		Multiply,
+		Divide,
+		Modulo
 	{
 	};
 }
-}
 
-#endif // !SL_OPERATORS_ARITHMETIC_HPP
+#endif
--- include/Simple-Utility/operators/bitwise.hpp
+++ include/Simple-Utility/operators/bitwise.hpp
@@ -4,162 +4,82 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef SL_OPERATORS_BINARY_HPP
-#define SL_OPERATORS_BINARY_HPP
+#ifndef SL_OPERATORS_BITWISE_HPP
+#define SL_OPERATORS_BITWISE_HPP
 
 #pragma once
 
-namespace sl {
-namespace operators {
+#include "Simple-Utility/concepts/bitwise.hpp"
 
-	template <class T>
-	struct BitwiseAND
+namespace sl::operators
+{
+	struct BitwiseAnd
 	{
-		friend constexpr T operator &(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::BitwiseAndAssignable<T1, T2>
+		friend constexpr T1 operator &(T1 lhs, const T2& rhs) noexcept(noexcept(lhs &= rhs))
 		{
-			T tmp(_lhs);
-			tmp &= _rhs;
-			return tmp;
+			lhs &= rhs;
+			return lhs;
 		}
 	};
 
-	template <class T>
-	struct BitwiseOR
+	struct BitwiseOr
 	{
-		friend constexpr T operator |(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::BitwiseOrAssignable<T1, T2>
+		friend constexpr T1 operator |(T1 lhs, const T2& rhs) noexcept(noexcept(lhs |= rhs))
 		{
-			T tmp(_lhs);
-			tmp |= _rhs;
-			return tmp;
+			lhs |= rhs;
+			return lhs;
 		}
 	};
 
-	template <class T>
-	struct BitwiseXOR
+	struct BitwiseXor
 	{
-		friend constexpr T operator ^(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::BitwiseXorAssignable<T1, T2>
+		friend constexpr T1 operator ^(T1 lhs, const T2& rhs) noexcept(noexcept(lhs ^= rhs))
 		{
-			T tmp(_lhs);
-			tmp ^= _rhs;
-			return tmp;
+			lhs ^= rhs;
+			return lhs;
 		}
 	};
 
-	template <class T>
 	struct BitwiseShiftLeft
 	{
-		friend constexpr T operator <<(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::BitwiseLeftShiftable<T1, T2>
+		friend constexpr T1 operator <<(T1 lhs, const T2& rhs) noexcept(noexcept(lhs <<= rhs))
 		{
-			T tmp(_lhs);
-			tmp <<= _rhs;
-			return tmp;
+			lhs <<= rhs;
+			return lhs;
 		}
 	};
 
-	template <class T>
 	struct BitwiseShiftRight
 	{
-		friend constexpr T operator >>(const T& _lhs, const T& _rhs)
+		template <class T1, class T2>
+		requires concepts::BitwiseRightShiftable<T1, T2>
+		friend constexpr T1 operator >>(T1 lhs, const T2& rhs) noexcept(noexcept(lhs >>= rhs))
 		{
-			T tmp(_lhs);
-			tmp >>= _rhs;
-			return tmp;
+			lhs >>= rhs;
+			return lhs;
 		}
 	};
 
-	template <class T>
-	struct BitwiseLogical : BitwiseAND<T>,
-		BitwiseOR<T>,
-		BitwiseXOR<T>
+	struct BitwiseLogical :
+		BitwiseAnd,
+		BitwiseOr,
+		BitwiseXor
 	{
 	};
 
-	template <class T>
-	struct BitwiseShift : BitwiseShiftRight<T>,
-		BitwiseShiftLeft<T>
-	{
-	};
-
-	template <class T>
-	struct Bitwise : BitwiseLogical<T>,
-		BitwiseShift<T>
-	{
-	};
-
-	template <class T1, class T2>
-	struct BitwiseAND2
-	{
-		friend constexpr T1 operator &(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp &= _rhs;
-			return tmp;
-		}
-	};
-
-	template <class T1, class T2>
-	struct BitwiseOR2
-	{
-		friend constexpr T1 operator |(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp |= _rhs;
-			return tmp;
-		}
-	};
-
-	template <class T1, class T2>
-	struct BitwiseXOR2
-	{
-		friend constexpr T1 operator ^(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp ^= _rhs;
-			return tmp;
-		}
-	};
-
-	template <class T1, class T2>
-	struct BitwiseShiftLeft2
-	{
-		friend constexpr T1 operator <<(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp <<= _rhs;
-			return tmp;
-		}
-	};
-
-	template <class T1, class T2>
-	struct BitwiseShiftRight2
-	{
-		friend constexpr T1 operator >>(const T1& _lhs, const T2& _rhs)
-		{
-			T1 tmp(_lhs);
-			tmp >>= _rhs;
-			return tmp;
-		}
-	};
-
-	template <class T1, class T2>
-	struct BitwiseLogical2 : BitwiseAND2<T1, T2>,
-		BitwiseOR2<T1, T2>,
-		BitwiseXOR2<T1, T2>
-	{
-	};
-
-	template <class T1, class T2>
-	struct BitwiseShift2 : BitwiseShiftRight2<T1, T2>,
-		BitwiseShiftLeft2<T1, T2>
-	{
-	};
-
-	template <class T1, class T2>
-	struct Bitwise2 : BitwiseLogical2<T1, T2>,
-		BitwiseShift2<T1, T2>
+	struct BitwiseShift :
+		BitwiseShiftRight,
+		BitwiseShiftLeft
 	{
 	};
 }
-}
 
-#endif // !SL_OPERATORS_BINARY_HPP
+#endif
