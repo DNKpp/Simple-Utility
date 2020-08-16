840dfd113333319e72cd7b2dc5c29d0346d547f1
DNKpp <DNKpp2011@gmail.com>
2020-08-17 00:26:22 +0200

    improve cmake setup
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/main.cpp
+++ test/main.cpp
@@ -7,12 +7,8 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
-#include <iostream>
-
 int main(int argc, char** argv)
 {
     int result = Catch::Session().run(argc, argv);
-
-    std::cin.get();
     return result;
 }
