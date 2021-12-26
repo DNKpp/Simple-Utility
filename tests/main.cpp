b204d0bf2c851d910735b1799a2cf9bf39742641
DNKpp <DNKpp2011@gmail.com>
2021-12-26 16:48:37 +0100

    remake project structure
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,10 @@
-
 //          Copyright Dominic Koepke 2019 - 2021.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
 #define CATCH_CONFIG_RUNNER
-#include "catch.hpp"
+#include <catch2/catch.hpp>
 
 int main(int argc, char** argv)
 {
