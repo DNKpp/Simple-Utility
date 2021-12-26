fd1de18f92407b836062ebd2cf864956f13e132a
DNKpp <DNKpp2011@gmail.com>
2021-12-26 20:06:42 +0100

    add _r variants for each shift operator and add those to existing test cases
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Utility/concepts/operators.hpp
+++ include/Simple-Utility/concepts/operators.hpp
@@ -18,8 +18,17 @@ namespace sl::concepts
 		{ lhs >> rhs };
 	};
 
+	template <class T1, class T2, class TResult = std::remove_cvref_t<T1>>
+	concept right_shiftable_with_r = requires(T1 lhs, T2 rhs)
+	{
+		{ lhs >> rhs } -> std::same_as<TResult>;
+	};
+
 	template <class T>
 	concept right_shiftable = right_shiftable_with<T, T>;
+
+	template <class T, class TResult = std::remove_cvref_t<T>>
+	concept right_shiftable_r = right_shiftable_with_r<T, T, TResult>;
 
 	template <class T1, class T2>
 	concept left_shiftable_with = requires(T1 lhs, T2 rhs)
@@ -27,14 +36,29 @@ namespace sl::concepts
 		{ lhs << rhs };
 	};
 
+	template <class T1, class T2, class TResult = std::remove_cvref_t<T1>>
+	concept left_shiftable_with_r = requires(T1 lhs, T2 rhs)
+	{
+		{ lhs << rhs } -> std::same_as<TResult>;
+	};
+
 	template <class T>
 	concept left_shiftable = left_shiftable_with<T, T>;
+
+	template <class T, class TResult = std::remove_cvref_t<T>>
+	concept left_shiftable_r = left_shiftable_with_r<T, T, TResult>;
 
 	template <class T1, class T2>
 	concept bidirectional_shiftable_with = left_shiftable_with<T1, T2> && right_shiftable_with<T1, T2>;
 
+	template <class T1, class T2, class TResult = std::remove_cvref_t<T1>>
+	concept bidirectional_shiftable_with_r = left_shiftable_with_r<T1, T2, TResult> && right_shiftable_with_r<T1, T2, TResult>;
+
 	template <class T>
 	concept bidirectional_shiftable = bidirectional_shiftable_with<T, T>;
+
+	template <class T, class TResult = std::remove_cvref_t<T>>
+	concept bidirectional_shiftable_r = bidirectional_shiftable_with_r<T, T, TResult>;
 
 	template <class T1, class T2>
 	concept right_shift_assignable_with = requires(T1 lhs, T2 rhs)
@@ -42,8 +66,17 @@ namespace sl::concepts
 		{ lhs >>= rhs };
 	};
 
-	template <class T1>
-	concept right_shift_assignable = right_shift_assignable_with<T1, T1>;
+	template <class T1, class T2, class TResult = std::remove_cvref_t<T1>&>
+	concept right_shift_assignable_with_r = requires(T1 lhs, T2 rhs)
+	{
+		{ lhs >>= rhs } -> std::same_as<TResult>;
+	};
+
+	template <class T>
+	concept right_shift_assignable = right_shift_assignable_with<T, T>;
+
+	template <class T, class TResult = std::remove_cvref_t<T>&>
+	concept right_shift_assignable_r = right_shift_assignable_with_r<T, T, TResult>;
 
 	template <class T1, class T2>
 	concept left_shift_assignable_with = requires(T1 lhs, T2 rhs)
@@ -51,20 +84,43 @@ namespace sl::concepts
 		{ lhs <<= rhs };
 	};
 
-	template <class T1>
-	concept left_shift_assignable = left_shift_assignable_with<T1, T1>;
+	template <class T1, class T2, class TResult = std::remove_cvref_t<T1>&>
+	concept left_shift_assignable_with_r = requires(T1 lhs, T2 rhs)
+	{
+		{ lhs <<= rhs } -> std::same_as<TResult>;
+	};
+
+	template <class T>
+	concept left_shift_assignable = left_shift_assignable_with<T, T>;
+
+	template <class T, class TResult = std::remove_cvref_t<T>&>
+	concept left_shift_assignable_r = left_shift_assignable_with_r<T, T, TResult>;
 
 	template <class T1, class T2>
 	concept bidirectional_shift_assignable_with = left_shift_assignable_with<T1, T2> && right_shift_assignable_with<T1, T2>;
 
-	template <class T1>
-	concept bidirectional_shift_assignable = bidirectional_shift_assignable_with<T1, T1>;
+	template <class T1, class T2, class TResult = std::remove_cvref_t<T1>&>
+	concept bidirectional_shift_assignable_with_r = left_shift_assignable_with_r<T1, T2, TResult>
+													&& right_shift_assignable_with_r<T1, T2, TResult>;
+
+	template <class T>
+	concept bidirectional_shift_assignable = bidirectional_shift_assignable_with<T, T>;
+
+	template <class T, class TResult = std::remove_cvref_t<T>&>
+	concept bidirectional_shift_assignable_r = bidirectional_shift_assignable_with_r<T, T, TResult>;
 
 	template <class T1, class T2>
 	concept fully_shiftable_with = bidirectional_shift_assignable_with<T1, T2> && bidirectional_shiftable_with<T1, T2>;
 
-	template <class T1>
-	concept fully_shiftable = fully_shiftable_with<T1, T1>;
+	template <class T1, class T2, class TResult = std::remove_cvref_t<T1>, class TAssignResult = std::remove_cvref_t<T1>&>
+	concept fully_shiftable_with_r = bidirectional_shiftable_with_r<T1, T2, TResult>
+									&& bidirectional_shift_assignable_with_r<T1, T2, TAssignResult>;
+
+	template <class T>
+	concept fully_shiftable = fully_shiftable_with<T, T>;
+
+	template <class T, class TResult = std::remove_cvref_t<T>, class TAssignResult = std::remove_cvref_t<T>&>
+	concept fully_shiftable_r = fully_shiftable_with_r<T, T, TResult, TAssignResult>;
 }
 
 #endif
--- tests/concepts/operators.cpp
+++ tests/concepts/operators.cpp
@@ -56,263 +56,473 @@ namespace
 	struct no_shift
 	{
 	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct right_shiftable_with_testable
+	{
+		static constexpr bool value{ right_shiftable_with<TLhs, TRhs> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct right_shiftable_with_r_testable
+	{
+		static constexpr bool value{ right_shiftable_with_r<TLhs, TRhs> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct right_shiftable_testable
+	{
+		static constexpr bool value{ right_shiftable<T> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct right_shiftable_r_testable
+	{
+		static constexpr bool value{ right_shiftable_r<T> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct left_shiftable_with_testable
+	{
+		static constexpr bool value{ left_shiftable_with<TLhs, TRhs> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct left_shiftable_with_r_testable
+	{
+		static constexpr bool value{ left_shiftable_with_r<TLhs, TRhs> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct left_shiftable_testable
+	{
+		static constexpr bool value{ left_shiftable<T> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct left_shiftable_r_testable
+	{
+		static constexpr bool value{ left_shiftable_r<T> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct bidirectional_shiftable_with_testable
+	{
+		static constexpr bool value{ bidirectional_shiftable_with<TLhs, TRhs> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct bidirectional_shiftable_with_r_testable
+	{
+		static constexpr bool value{ bidirectional_shiftable_with_r<TLhs, TRhs> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct bidirectional_shiftable_testable
+	{
+		static constexpr bool value{ bidirectional_shiftable<T> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct bidirectional_shiftable_r_testable
+	{
+		static constexpr bool value{ bidirectional_shiftable_r<T> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct right_shift_assignable_with_testable
+	{
+		static constexpr bool value{ right_shift_assignable_with<TLhs, TRhs> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct right_shift_assignable_with_r_testable
+	{
+		static constexpr bool value{ right_shift_assignable_with_r<TLhs, TRhs> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct right_shift_assignable_testable
+	{
+		static constexpr bool value{ right_shift_assignable<T> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct right_shift_assignable_r_testable
+	{
+		static constexpr bool value{ right_shift_assignable_r<T> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct left_shift_assignable_with_testable
+	{
+		static constexpr bool value{ left_shift_assignable_with<TLhs, TRhs> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct left_shift_assignable_with_r_testable
+	{
+		static constexpr bool value{ left_shift_assignable_with_r<TLhs, TRhs> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct left_shift_assignable_testable
+	{
+		static constexpr bool value{ left_shift_assignable<T> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct left_shift_assignable_r_testable
+	{
+		static constexpr bool value{ left_shift_assignable_r<T> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct bidirectional_shift_assignable_with_testable
+	{
+		static constexpr bool value{ bidirectional_shift_assignable_with<TLhs, TRhs> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct bidirectional_shift_assignable_with_r_testable
+	{
+		static constexpr bool value{ bidirectional_shift_assignable_with_r<TLhs, TRhs> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct bidirectional_shift_assignable_testable
+	{
+		static constexpr bool value{ bidirectional_shift_assignable<T> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct bidirectional_shift_assignable_r_testable
+	{
+		static constexpr bool value{ bidirectional_shift_assignable_r<T> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct fully_shiftable_with_testable
+	{
+		static constexpr bool value{ fully_shiftable_with<TLhs, TRhs> == VResult };
+	};
+
+	template <class TLhs, class TRhs, bool VResult>
+	struct fully_shiftable_with_r_testable
+	{
+		static constexpr bool value{ fully_shiftable_with_r<TLhs, TRhs> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct fully_shiftable_testable
+	{
+		static constexpr bool value{ fully_shiftable<T> == VResult };
+	};
+
+	template <class T, bool VResult>
+	struct fully_shiftable_r_testable
+	{
+		static constexpr bool value{ fully_shiftable_r<T> == VResult };
+	};
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"right_shiftable_with should determine if two types can be used within an operator >> expression.",
+	"right_shiftable_with(_r) should determine if two types can be used within an operator >> expression.",
 	"[concepts][operators]",
 	((class TLhs, class TRhs, bool VExpected), TLhs, TRhs, VExpected),
-	(int, int, true),
-	(float, int, false),
-	(int, float, false),
-	(shift_target<right>, int, true),
-	(const shift_target<right>, int, true),
-	(shift_target<right>, no_shift, false),
-	(shift_target<>, int, false)
+	(right_shiftable_with_testable, right_shiftable_with_r_testable),
+	(
+		(int, int, true),
+		(float, int, false),
+		(int, float, false),
+		(shift_target<right>, int, true),
+		(const shift_target<right>, int, true),
+		(shift_target<right>, no_shift, false),
+		(shift_target<>, int, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(right_shiftable_with<TLhs, TRhs> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"right_shiftable should determine if a type can be used within an operator >> expression.",
+	"right_shiftable(_r) should determine if a type can be used within an operator >> expression.",
 	"[concepts][operators]",
 	((class T, bool VExpected), T, VExpected),
-	(int, true),
-	(float, false),
-	(shift_target<right>, false),
-	(const shift_target<right>, false),
-	(shift_target<>, false)
+	(right_shiftable_testable, right_shiftable_r_testable),
+	(
+		(int, true),
+		(float, false),
+		(shift_target<right>, false),
+		(const shift_target<right>, false),
+		(shift_target<>, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(right_shiftable<T> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"left_shiftable_with should determine if two types can be used within an operator << expression.",
+	"left_shiftable_with(_r) should determine if two types can be used within an operator << expression.",
 	"[concepts][operators]",
 	((class TLhs, class TRhs, bool VExpected), TLhs, TRhs, VExpected),
-	(int, int, true),
-	(float, int, false),
-	(int, float, false),
-	(shift_target<left>, int, true),
-	(const shift_target<left>, int, true),
-	(shift_target<left>, no_shift, false),
-	(shift_target<>, int, false)
+	(left_shiftable_with_testable, left_shiftable_with_r_testable),
+	(
+		(int, int, true),
+		(float, int, false),
+		(int, float, false),
+		(shift_target<left>, int, true),
+		(const shift_target<left>, int, true),
+		(shift_target<left>, no_shift, false),
+		(shift_target<>, int, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(left_shiftable_with<TLhs, TRhs> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"left_shiftable should determine if a type can be used within an operator << expression.",
+	"left_shiftable(_r) should determine if a type can be used within an operator << expression.",
 	"[concepts][operators]",
 	((class T, bool VExpected), T, VExpected),
-	(int, true),
-	(float, false),
-	(shift_target<left>, false),
-	(const shift_target<left>, false),
-	(shift_target<>, false)
+	(left_shiftable_testable, left_shiftable_r_testable),
+	(
+		(int, true),
+		(float, false),
+		(shift_target<left>, false),
+		(const shift_target<left>, false),
+		(shift_target<>, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(left_shiftable<T> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"bidirectional_shiftable_with should determine if two types can be used within operator << and operator >> expressions.",
+	"bidirectional_shiftable_with(_r) should determine if two types can be used within operator << and operator >> expressions.",
 	"[concepts][operators]",
 	((class TLhs, class TRhs, bool VExpected), TLhs, TRhs, VExpected),
-	(int, int, true),
-	(float, int, false),
-	(int, float, false),
-	(shift_target<bidirectional>, int, true),
-	(const shift_target<bidirectional>, int, true),
-	(shift_target<left>, int, false),
-	(shift_target<right>, int, false)
+	(bidirectional_shiftable_with_testable, bidirectional_shiftable_with_r_testable),
+	(
+		(int, int, true),
+		(float, int, false),
+		(int, float, false),
+		(shift_target<bidirectional>, int, true),
+		(const shift_target<bidirectional>, int, true),
+		(shift_target<left>, int, false),
+		(shift_target<right>, int, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(bidirectional_shiftable_with<TLhs, TRhs> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"bidirectional_shiftable should determine if a type can be used within operator << and operator >> expressions.",
+	"bidirectional_shiftable(_r) should determine if a type can be used within operator << and operator >> expressions.",
 	"[concepts][operators]",
 	((class T, bool VExpected), T, VExpected),
-	(int, true),
-	(float, false),
-	(shift_target<bidirectional>, false),
-	(const shift_target<bidirectional>, false),
-	(shift_target<left>, false),
-	(shift_target<right>, false)
+	(bidirectional_shiftable_testable, bidirectional_shiftable_r_testable),
+	(
+		(int, true),
+		(float, false),
+		(shift_target<bidirectional>, false),
+		(const shift_target<bidirectional>, false),
+		(shift_target<left>, false),
+		(shift_target<right>, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(bidirectional_shiftable<T> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"right_shift_assignable_with should determine if two types can be used within an operator >>= expression.",
+	"right_shift_assignable_with(_r) should determine if two types can be used within an operator >>= expression.",
 	"[concepts][operators]",
 	((class TLhs, class TRhs, bool VExpected), TLhs, TRhs, VExpected),
-	(int, int, true),
-	(float, int, false),
-	(int, float, false),
-	(shift_target<right_assign>, int, true),
-	(const shift_target<right_assign>, int, false),
-	(shift_target<right_assign>, no_shift, false),
-	(shift_target<>, int, false)
+	(right_shift_assignable_with_testable, right_shift_assignable_with_r_testable),
+	(
+		(int, int, true),
+		(float, int, false),
+		(int, float, false),
+		(shift_target<right_assign>, int, true),
+		(const shift_target<right_assign>, int, false),
+		(shift_target<right_assign>, no_shift, false),
+		(shift_target<>, int, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(right_shift_assignable_with<TLhs, TRhs> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"right_shift_assignable should determine if a type can be used within an operator >>= expression.",
+	"right_shift_assignable(_r) should determine if a type can be used within an operator >>= expression.",
 	"[concepts][operators]",
 	((class T, bool VExpected), T, VExpected),
-	(int, true),
-	(float, false),
-	(shift_target<right_assign>, false),
-	(const shift_target<right_assign>, false),
-	(shift_target<>, false)
+	(right_shift_assignable_testable, right_shift_assignable_r_testable),
+	(
+		(int, true),
+		(float, false),
+		(shift_target<right_assign>, false),
+		(const shift_target<right_assign>, false),
+		(shift_target<>, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(right_shift_assignable<T> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"left_shift_assignable_with should determine if two types can be used within an operator <<= expression.",
+	"left_shift_assignable_with(_r) should determine if two types can be used within an operator <<= expression.",
 	"[concepts][operators]",
 	((class TLhs, class TRhs, bool VExpected), TLhs, TRhs, VExpected),
-	(int, int, true),
-	(float, int, false),
-	(int, float, false),
-	(shift_target<left_assign>, int, true),
-	(const shift_target<left_assign>, int, false),
-	(shift_target<left_assign>, no_shift, false),
-	(shift_target<>, int, false)
+	(left_shift_assignable_with_testable, left_shift_assignable_with_r_testable),
+	(
+		(int, int, true),
+		(float, int, false),
+		(int, float, false),
+		(shift_target<left_assign>, int, true),
+		(const shift_target<left_assign>, int, false),
+		(shift_target<left_assign>, no_shift, false),
+		(shift_target<>, int, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(left_shift_assignable_with<TLhs, TRhs> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"left_shift_assignable should determine if a type can be used within an operator <<= expression.",
+	"left_shift_assignable(_r) should determine if a type can be used within an operator <<= expression.",
 	"[concepts][operators]",
 	((class T, bool VExpected), T, VExpected),
-	(int, true),
-	(float, false),
-	(shift_target<right_assign>, false),
-	(const shift_target<right_assign>, false),
-	(shift_target<>, false)
+	(left_shift_assignable_testable, left_shift_assignable_r_testable),
+	(
+		(int, true),
+		(float, false),
+		(shift_target<right_assign>, false),
+		(const shift_target<right_assign>, false),
+		(shift_target<>, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(left_shift_assignable<T> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"bidirectional_shift_assignable_with should determine if two types can be used within operator <<= and operator >>= expressions.",
+	"bidirectional_shift_assignable_with(_r) should determine if two types can be used within operator <<= and operator >>= expressions.",
 	"[concepts][operators]",
 	((class TLhs, class TRhs, bool VExpected), TLhs, TRhs, VExpected),
-	(int, int, true),
-	(float, int, false),
-	(int, float, false),
-	(shift_target<bidirectional_assign>, int, true),
-	(const shift_target<bidirectional_assign>, int, false),
-	(shift_target<left_assign>, int, false),
-	(shift_target<right_assign>, int, false)
+	(bidirectional_shift_assignable_with_testable, bidirectional_shift_assignable_with_r_testable),
+	(
+		(int, int, true),
+		(float, int, false),
+		(int, float, false),
+		(shift_target<bidirectional_assign>, int, true),
+		(const shift_target<bidirectional_assign>, int, false),
+		(shift_target<left_assign>, int, false),
+		(shift_target<right_assign>, int, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(bidirectional_shift_assignable_with<TLhs, TRhs> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"bidirectional_shift_assignable should determine if a type can be used within operator <<= and operator =>> expressions.",
+	"bidirectional_shift_assignable(_r) should determine if a type can be used within operator <<= and operator =>> expressions.",
 	"[concepts][operators]",
 	((class T, bool VExpected), T, VExpected),
-	(int, true),
-	(float, false),
-	(shift_target<bidirectional_assign>, false),
-	(const shift_target<bidirectional_assign>, false),
-	(shift_target<left_assign>, false),
-	(shift_target<right_assign>, false)
+	(bidirectional_shift_assignable_testable, bidirectional_shift_assignable_r_testable),
+	(
+		(int, true),
+		(float, false),
+		(shift_target<bidirectional_assign>, false),
+		(const shift_target<bidirectional_assign>, false),
+		(shift_target<left_assign>, false),
+		(shift_target<right_assign>, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(bidirectional_shift_assignable<T> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"fully_shiftable_with should determine if two types can be used within all shift operator expressions.",
+	"fully_shiftable_with(_r) should determine if two types can be used within all shift operator expressions.",
 	"[concepts][operators]",
 	((class TLhs, class TRhs, bool VExpected), TLhs, TRhs, VExpected),
-	(int, int, true),
-	(float, int, false),
-	(int, float, false),
-	(shift_target<fully>, int, true),
-	(const shift_target<fully>, int, false),
-	(shift_target<fully ^ right>, int, false),
-	(shift_target<fully ^ left>, int, false),
-	(shift_target<fully ^ right_assign>, int, false),
-	(shift_target<fully ^ left_assign>, int, false)
+	(fully_shiftable_with_testable, fully_shiftable_with_r_testable),
+	(
+		(int, int, true),
+		(float, int, false),
+		(int, float, false),
+		(shift_target<fully>, int, true),
+		(const shift_target<fully>, int, false),
+		(shift_target<fully ^ right>, int, false),
+		(shift_target<fully ^ left>, int, false),
+		(shift_target<fully ^ right_assign>, int, false),
+		(shift_target<fully ^ left_assign>, int, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(fully_shiftable_with<TLhs, TRhs> == VExpected);
+	REQUIRE(TestType::value);
 }
 
 #pragma warning(disable: 26444)
-TEMPLATE_TEST_CASE_SIG
+TEMPLATE_PRODUCT_TEST_CASE_SIG
 (
-	"fully_shiftable should determine if a type can be used within all shift operator expressions.",
+	"fully_shiftable(_r) should determine if a type can be used within all shift operator expressions.",
 	"[concepts][operators]",
 	((class T, bool VExpected), T, VExpected),
-	(int, true),
-	(float, false),
-	(shift_target<fully>, false),
-	(const shift_target<fully>, false),
-	(shift_target<fully ^ right>, false),
-	(shift_target<fully ^ left>, false),
-	(shift_target<fully ^ right_assign>, false),
-	(shift_target<fully ^ left_assign>, false)
+	(fully_shiftable_testable, fully_shiftable_r_testable),
+	(
+		(int, true),
+		(float, false),
+		(shift_target<fully>, false),
+		(const shift_target<fully>, false),
+		(shift_target<fully ^ right>, false),
+		(shift_target<fully ^ left>, false),
+		(shift_target<fully ^ right_assign>, false),
+		(shift_target<fully ^ left_assign>, false)
+	)
 )
 #pragma warning(default: 26444)
 {
-	REQUIRE(fully_shiftable<T> == VExpected);
+	REQUIRE(TestType::value);
 }
