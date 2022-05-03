6d2f05451cda36e23d0b4fb516678fd2afd9f3d3
DNKpp <DNKpp2011@gmail.com>
2022-05-03 18:37:40 +0200

    fix: add missing license header in unique_handle.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Utility/unique_handle.hpp
+++ include/Simple-Utility/unique_handle.hpp
@@ -1,3 +1,8 @@
+//          Copyright Dominic Koepke 2019 - 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef SL_UNIQUE_HANDLE_HPP
 #define SL_UNIQUE_HANDLE_HPP
 
