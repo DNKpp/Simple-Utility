d42b5ac6dc3f1a938a24f7f84e9ea36afd5eb6c8
DNKpp <DNKpp2011@gmail.com>
2022-06-17 20:24:07 +0200

    deps: update catch2 to version 3.0.1
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- tests/functional.cpp
+++ tests/functional.cpp
@@ -3,7 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
 #include "Simple-Utility/functional.hpp"
 
