f75ecdaa2134a6ea9f6b6ce926b9805b7c2f4287
DNKpp <DNKpp2011@gmail.com>
2022-06-17 20:18:10 +0200

    env: add predicate file structure
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ include/Simple-Utility/predicate.hpp
@@ -0,0 +1,22 @@
+//          Copyright Dominic Koepke 2019 - 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef SL_UTILITY_PREDICATE_HPP
+#define SL_UTILITY_PREDICATE_HPP
+
+#pragma once
+
+#include "Simple-Utility/functional.hpp"
+
+namespace sl::functional::detail
+{
+}
+
+namespace sl::functional
+{
+
+}
+
+#endif
---
+++ tests/predicate.cpp
@@ -0,0 +1,10 @@
+//          Copyright Dominic Koepke 2019 - 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include <catch2/catch.hpp>
+
+#include "Simple-Utility/predicate.hpp"
+
+using namespace sl;
