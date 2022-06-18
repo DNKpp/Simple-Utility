0afa8ee6862828b1eda91b64397d0cc8369c5e64
DNKpp <DNKpp2011@gmail.com>
2022-06-18 12:01:52 +0200

    add&test: disjunction_operator and conjunction_operator for predicate_fn
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Utility/functional/predicate.hpp
+++ include/Simple-Utility/functional/predicate.hpp
@@ -12,11 +12,185 @@
 
 namespace sl::functional::detail
 {
+	struct binary_conjunction_fn
+	{
+		template <class TFunc1, class TFunc2, class... TArgs>
+		[[nodiscard]]
+		constexpr decltype(auto) operator ()
+		(
+			TFunc1&& func1,
+			TFunc2&& func2,
+			const TArgs&... v
+		) const
+		noexcept(noexcept(std::invoke(std::forward<TFunc1>(func1), v...))
+				&& noexcept(std::invoke(std::forward<TFunc2>(func2), v...))
+		)
+			requires std::predicate<TFunc1, TArgs...>
+					&& std::predicate<TFunc2, TArgs...>
+		{
+			return std::invoke(std::forward<TFunc1>(func1), v...)
+					&& std::invoke(std::forward<TFunc2>(func2), v...);
+		}
+	};
 
+	struct binary_disjunction_fn
+	{
+		template <class TFunc1, class TFunc2, class... TArgs>
+		[[nodiscard]]
+		constexpr decltype(auto) operator ()
+		(
+			TFunc1&& func1,
+			TFunc2&& func2,
+			const TArgs&... v
+		) const
+		noexcept(noexcept(std::invoke(std::forward<TFunc1>(func1), v...))
+				&& noexcept(std::invoke(std::forward<TFunc2>(func2), v...))
+		)
+			requires std::predicate<TFunc1, TArgs...>
+					&& std::predicate<TFunc2, TArgs...>
+		{
+			return std::invoke(std::forward<TFunc1>(func1), v...)
+					|| std::invoke(std::forward<TFunc2>(func2), v...);
+		}
+	};
 }
 
 namespace sl::functional
 {
+	template <class TDerived, template <class> class TClosureBase>
+		requires std::is_class_v<TDerived>
+	struct conjunction_operator
+	{
+	private:
+		using composer_t = detail::composer<TClosureBase, detail::binary_conjunction_fn>;
+
+	public:
+		template <class TOther>
+		[[nodiscard]]
+		constexpr auto operator &&
+		(
+			TOther&& other
+		) && noexcept(noexcept(composer_t::compose(std::declval<TDerived&&>(), std::declval<TOther>())))
+		{
+			return composer_t::compose(static_cast<TDerived&&>(*this), std::forward<TOther>(other));
+		}
+
+		template <class TOther>
+		[[nodiscard]]
+		constexpr auto operator &&
+		(
+			TOther&& other
+		) const & noexcept(noexcept(composer_t::compose(std::declval<const TDerived&>(), std::declval<TOther>())))
+		{
+			return composer_t::compose(static_cast<const TDerived&>(*this), std::forward<TOther>(other));
+		}
+
+		template <class TLhs>
+		[[nodiscard]]
+		friend constexpr auto operator &&
+		(
+			TLhs&& lhs,
+			conjunction_operator&& rhs
+		)
+		noexcept(noexcept(composer_t::compose(std::declval<TLhs>(), std::declval<TDerived&&>())))
+			requires (!requires { lhs.operator|(std::move(rhs)); })
+
+		{
+			return composer_t::compose(std::forward<TLhs>(lhs), static_cast<TDerived&&>(rhs));
+		}
+
+		template <class TLhs>
+		[[nodiscard]]
+		friend constexpr auto operator &&
+		(
+			TLhs&& lhs,
+			const conjunction_operator& rhs
+		)
+		noexcept(noexcept(composer_t::compose(std::declval<TLhs>(), std::declval<const TDerived&>())))
+			requires (!requires { lhs.operator|(rhs); })
+		{
+			return composer_t::compose(std::forward<TLhs>(lhs), static_cast<const TDerived&>(rhs));
+		}
+	};
+
+	template <class TDerived, template <class> class TClosureBase>
+		requires std::is_class_v<TDerived>
+	struct disjunction_operator
+	{
+	private:
+		using composer_t = detail::composer<TClosureBase, detail::binary_disjunction_fn>;
+
+	public:
+		template <class TOther>
+		[[nodiscard]]
+		constexpr auto operator ||
+		(
+			TOther&& other
+		) && noexcept(noexcept(composer_t::compose(std::declval<TDerived&&>(), std::declval<TOther>())))
+		{
+			return composer_t::compose(static_cast<TDerived&&>(*this), std::forward<TOther>(other));
+		}
+
+		template <class TOther>
+		[[nodiscard]]
+		constexpr auto operator ||
+		(
+			TOther&& other
+		) const & noexcept(noexcept(composer_t::compose(std::declval<const TDerived&>(), std::declval<TOther>())))
+		{
+			return composer_t::compose(static_cast<const TDerived&>(*this), std::forward<TOther>(other));
+		}
+
+		template <class TLhs>
+		[[nodiscard]]
+		friend constexpr auto operator ||
+		(
+			TLhs&& lhs,
+			disjunction_operator&& rhs
+		)
+		noexcept(noexcept(composer_t::compose(std::declval<TLhs>(), std::declval<TDerived&&>())))
+			requires (!requires { lhs.operator|(std::move(rhs)); })
+
+		{
+			return composer_t::compose(std::forward<TLhs>(lhs), static_cast<TDerived&&>(rhs));
+		}
+
+		template <class TLhs>
+		[[nodiscard]]
+		friend constexpr auto operator ||
+		(
+			TLhs&& lhs,
+			const disjunction_operator& rhs
+		)
+		noexcept(noexcept(composer_t::compose(std::declval<TLhs>(), std::declval<const TDerived&>())))
+			requires (!requires { lhs.operator|(rhs); })
+		{
+			return composer_t::compose(std::forward<TLhs>(lhs), static_cast<const TDerived&>(rhs));
+		}
+	};
+
+	/**
+	 * \brief Helper type which accepts a functional type and enables pipe, conjunctive and disjunctive chaining.
+	 * \tparam TFunc The functional type.
+	 */
+	template <class TFunc>
+	class predicate_fn
+		: public closure_base_fn<std::remove_cvref_t<TFunc>>,
+		public pipe_operator<predicate_fn<TFunc>, predicate_fn>,
+		public conjunction_operator<predicate_fn<TFunc>, predicate_fn>,
+		public disjunction_operator<predicate_fn<TFunc>, predicate_fn>
+	{
+		using closure_t = closure_base_fn<std::remove_cvref_t<TFunc>>;
+	public:
+		using closure_t::closure_base_fn;
+	};
+
+	/**
+	 * \brief Deduction guide.
+	 * \tparam TFunc Type of the given functional.
+	 */
+	template <class TFunc>
+	predicate_fn(TFunc) -> predicate_fn<std::remove_cvref_t<TFunc>>;
 
 }
 
--- tests/functional/predicate.cpp
+++ tests/functional/predicate.cpp
@@ -4,7 +4,83 @@
 //          https://www.boost.org/LICENSE_1_0.txt)
 
 #include <catch2/catch_template_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include "Simple-Utility/functional/predicate.hpp"
 
 using namespace sl;
+
+constexpr auto trueFunc = [](auto&&...) { return true; };
+constexpr auto falseFunc = [](auto&&...) { return false; };
+
+TEST_CASE("predicate_fn is composable with operator && as left-hand-side", "[functional][predicate]")
+{
+	const auto& [chainedFunction, expectedResult] = GENERATE(table<bool(*)(), bool>({ { trueFunc, true }, { falseFunc, false } }));
+
+	const functional::predicate_fn composedPredicate = functional::predicate_fn{ trueFunc } && chainedFunction;
+
+	REQUIRE(composedPredicate() == expectedResult);
+}
+
+TEST_CASE("predicate_fn is composable with operator && as right-hand-side", "[functional][predicate]")
+{
+	const auto& [chainedFunction, expectedResult] = GENERATE(table<bool(*)(), bool>({ { trueFunc, true }, { falseFunc, false } }));
+
+	const functional::predicate_fn composedPredicate = chainedFunction && functional::predicate_fn{ trueFunc };
+
+	REQUIRE(composedPredicate() == expectedResult);
+}
+
+TEST_CASE("predicate_fn is composable with operator || as left-hand-side", "[functional][predicate]")
+{
+	const auto& [chainedFunction, expectedResult] = GENERATE(table<bool(*)(), bool>({ { trueFunc, true }, { falseFunc, false } }));
+
+	const functional::predicate_fn composedPredicate = functional::predicate_fn{ falseFunc } || chainedFunction;
+
+	REQUIRE(composedPredicate() == expectedResult);
+}
+
+TEST_CASE("predicate_fn is composable with operator || as right-hand-side", "[functional][predicate]")
+{
+	const auto& [chainedFunction, expectedResult] = GENERATE(table<bool(*)(), bool>({ { trueFunc, true }, { falseFunc, false } }));
+
+	const functional::predicate_fn composedPredicate = chainedFunction || functional::predicate_fn{ falseFunc };
+
+	REQUIRE(composedPredicate() == expectedResult);
+}
+
+TEST_CASE("predicate_fn follows the common operator hierarchy.", "[functional][predicate]")
+{
+	const auto& [andFunc, orFunc, expectedResult] = GENERATE(
+		table<bool(*)(),
+		bool(*)(),
+		bool>({
+			{ falseFunc, falseFunc, false },
+			{ falseFunc, trueFunc, true },
+			{ trueFunc, falseFunc, true },
+			{ trueFunc, trueFunc, true },
+		})
+	);
+
+	const functional::predicate_fn composedPredicate = functional::predicate_fn{ trueFunc } && andFunc || orFunc;
+
+	REQUIRE(composedPredicate() == expectedResult);
+}
+
+TEST_CASE("predicate_fn common operator hierarchy can be adjusted with ().", "[functional][predicate]")
+{
+	const auto& [andFunc, orFunc, expectedResult] = GENERATE(
+		table<bool(*)(),
+		bool(*)(),
+		bool>({
+			{ falseFunc, falseFunc, false },
+			{ falseFunc, trueFunc, false },
+			{ trueFunc, falseFunc, false },
+			{ trueFunc, trueFunc, true },
+		})
+	);
+
+	const functional::predicate_fn composedPredicate = (functional::predicate_fn{ falseFunc } || orFunc) && andFunc;
+
+	REQUIRE(composedPredicate() == expectedResult);
+}
