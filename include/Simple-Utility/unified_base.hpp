ec275250c7c22f4330213f9f3245a5008331ee95
DNKpp <DNKpp2011@gmail.com>
2022-06-24 15:45:55 +0200

    docs: add documentation in unified_base.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Utility/unified_base.hpp
+++ include/Simple-Utility/unified_base.hpp
@@ -12,10 +12,23 @@
 
 namespace sl
 {
+	/**
+	 * \brief Helper type, which can be used to create a unified base class for templated derived classes and thus
+	 * getting an straight-forward way of disambiguing binary operators.
+	 * \tparam TTag The provided tag type.
+	 */
 	template <class TTag>
+		requires std::is_class_v<TTag> && std::same_as<TTag, std::remove_cvref_t<TTag>>
 	struct unified_base
 	{};
 
+	/**
+	 * \brief Determines whether T is derived of ``unified_base`` with the provided tag type.
+	 * \tparam T The type to check.
+	 * \tparam TTag The tag type to check for.
+	 * \note This concepts makes use of ``std::is_base_of_v`` instead of ``std::derived_from`` because the former detects
+	 * private inheritance, which is in fact the intended inheritance type.
+	 */
 	template <class T, class TTag>
 	concept derived_from_unified_base = std::is_base_of_v<unified_base<TTag>, std::remove_cvref_t<T>>; 
 }
