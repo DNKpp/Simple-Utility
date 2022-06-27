785110f3649a439e5a45e8075aa78b02e28513ea
DNKpp <DNKpp2011@gmail.com>
2022-06-28 01:08:57 +0200

    refactor: rework disjunction.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Utility/functional/operators/disjunction.hpp
+++ include/Simple-Utility/functional/operators/disjunction.hpp
@@ -8,7 +8,6 @@
 
 #pragma once
 
-#include "Simple-Utility/unified_base.hpp"
 #include "Simple-Utility/functional/base.hpp"
 
 namespace sl::functional::operators::detail
@@ -35,96 +34,57 @@ namespace sl::functional::operators::detail
 			);
 		}
 	};
-
-	struct disjunction_base_tag
-	{};
 }
 
 namespace sl::functional::operators
 {
 	/**
-	 * \addtogroup GROUP_FUNCTIONAL_OPERATORS operators
+	 * \addtogroup GROUP_FUNCTIONAL_OPERATORS
 	 * \ingroup GROUP_FUNCTIONAL
 	 * @{
 	 */
 
 	/**
-	 * \brief Helper type which enables disjunctive composing of two functional objects via operator ||.
-	 * \tparam TDerived The most derived class type.
-	 * \tparam TClosureBase The base closure type (with one template argument left to be specified).
+	 * \brief Tag type which enables disjunctive composing of two functional objects via operator ||.
+	 * \relatesalso sl::functional::enable_operation
 	 */
-	template <class TDerived, template <class> class TClosureBase>
-		requires std::is_class_v<TDerived> && std::same_as<TDerived, std::remove_cvref_t<TDerived>>
 	struct disjunction
-		: private unified_base<detail::disjunction_base_tag>
+	{};
+
+	/**
+	 * \brief Specialized traits for \ref disjunction.
+	 * \relatesalso disjunction
+	 */
+	template <>
+	struct tag_traits<disjunction>
 	{
-	private:
-		using composer_t = detail::compose_helper_t<TClosureBase, detail::disjunction_caller_fn>;
-
-	public:
-		/**
-		 * \brief Composes this and the other functional object as disjunction.
-		 * \tparam TOther The type of the other functional object.
-		 * \param other The right-hand-side functional object.
-		 * \return The disjunctive composition of this and other as new functional object.
-		 */
-		template <class TOther>
-		[[nodiscard]]
-		constexpr auto operator ||
-		(
-			TOther&& other
-		) && noexcept(std::is_nothrow_invocable_v<composer_t, TDerived&&, TOther>)
-		{
-			return composer_t{}(static_cast<TDerived&&>(*this), std::forward<TOther>(other));
-		}
-
-		/**
-		 * \copydoc operator||()
-		 */
-		template <class TOther>
-		[[nodiscard]]
-		constexpr auto operator ||
-		(
-			TOther&& other
-		) const & noexcept(std::is_nothrow_invocable_v<composer_t, const TDerived&, TOther>)
-		{
-			return composer_t{}(static_cast<const TDerived&>(*this), std::forward<TOther>(other));
-		}
-
-		/**
-		 * \brief Composes both functional objects as disjunction.
-		 * \tparam TLhs The left-hand-side functional type.
-		 * \param lhs The left-hand-side functional object.
-		 * \param rhs The right-hand-side functional object.
-		 * \return The disjunctive composition of both functional objects as new functional object.
-		 */
-		template <class TLhs>
-			requires (!derived_from_unified_base<TLhs, detail::disjunction_base_tag>)
-		[[nodiscard]]
-		friend constexpr auto operator ||
-		(
-			TLhs&& lhs,
-			disjunction&& rhs
-		) noexcept(std::is_nothrow_invocable_v<composer_t, TLhs, TDerived&&>)
-		{
-			return composer_t{}(std::forward<TLhs>(lhs), static_cast<TDerived&&>(rhs));
-		}
-
-		/**
-		 * \copydoc operator||(TLhs&&, disjunction&&)
-		 */
-		template <class TLhs>
-			requires (!derived_from_unified_base<TLhs, detail::disjunction_base_tag>)
-		[[nodiscard]]
-		friend constexpr auto operator ||
-		(
-			TLhs&& lhs,
-			const disjunction& rhs
-		) noexcept(std::is_nothrow_invocable_v<composer_t, TLhs, const TDerived&>)
-		{
-			return composer_t{}(std::forward<TLhs>(lhs), static_cast<const TDerived&>(rhs));
-		}
+		using operation_t = detail::disjunction_caller_fn;
 	};
+
+	/**
+	 * \brief Composes both functional objects as disjunction.
+	 * \relatesalso disjunction
+	 * \tparam TLhs The left-hand-side functional type.
+	 * \tparam TRhs The right-hand-side functional type.
+	 * \param lhs The left-hand-side functional object.
+	 * \param rhs The right-hand-side functional object.
+	 * \return The disjunctive composition of both functional objects as new functional object.
+	 */
+	template <class TLhs, class TRhs>
+		requires (std::derived_from<std::remove_cvref_t<TLhs>, disjunction>
+				&& derived_from_unified_base<TLhs, functional::detail::enable_operators_base_tag>)
+				|| (std::derived_from<std::remove_cvref_t<TRhs>, disjunction>
+					&& derived_from_unified_base<TRhs, functional::detail::enable_operators_base_tag>)
+	[[nodiscard]]
+	constexpr auto operator ||
+	(
+		TLhs&& lhs,
+		TRhs&& rhs
+	)
+	noexcept(is_nothrow_composable_v<disjunction, TLhs, TRhs>)
+	{
+		return functional::detail::make_composition_from_tag<disjunction>(std::forward<TLhs>(lhs), std::forward<TRhs>(rhs));
+	}
 
 	/** @} */
 }
