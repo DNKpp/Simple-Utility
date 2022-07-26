9e072b2b34bb96318b191ab754c4c1467b95ea9f
DNKpp <dnkpp2011@gmail.com>
2022-07-26 14:05:03 +0200

    test: add tests for supported of third-party types

--- tests/nullables/additional.cpp
+++ tests/nullables/additional.cpp
@@ -7,3 +7,27 @@
 
 // test all-in-one include here
 #include "Simple-Utility/nullables.hpp"
+
+using namespace sl::nullables;
+
+TEMPLATE_TEST_CASE(
+	"provided types satisfy the input_nullables requirements",
+	"[nullables][concept]",
+	std::unique_ptr<int>,
+	std::shared_ptr<int>,
+	std::optional<int>
+)
+{
+	STATIC_REQUIRE(input_nullable<TestType>);
+}
+
+TEMPLATE_TEST_CASE(
+	"provided types satisfy the nullables requirements",
+	"[nullables][concept]",
+	std::unique_ptr<int>,
+	std::shared_ptr<int>,
+	std::optional<int>
+)
+{
+	STATIC_REQUIRE(nullable<TestType>);
+}
