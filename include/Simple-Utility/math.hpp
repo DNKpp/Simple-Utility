957b5c14d102548fbfc600c7c6ea40a451b375b0
DNKpp <dnkpp2011@gmail.com>
2022-09-26 01:11:42 +0200

    feat: add math::remquo

--- include/Simple-Utility/math.hpp
+++ include/Simple-Utility/math.hpp
@@ -24,6 +24,44 @@ namespace sl::math
 	*/
 
 	/**
+	 * \brief Result type for the remquo operation.
+	 * \tparam T Used floating point type
+	 */
+	template <std::floating_point T>
+	struct remquo_result
+	{
+		T remainder;
+		int quo;
+	};
+
+	/**
+	 * \brief Helper alias, deducing the result type of a ``std::remquo`` call.
+	 */
+	template <class T>
+	using remquo_result_value_type = decltype(std::remquo(std::declval<T>(), std::declval<T>(), nullptr));
+
+	/**
+	 * \brief Computes the floating-point remainder of the division operation x/y. Additionally, the sign and at least the three of the
+	 * last bits of x/y will be determined, sufficient to determine the octant of the result within a period.
+	 * \tparam T  Used floating point type
+	 * \param x First floating point value.
+	 * \param y Second floating point value.
+	 * \attention y must not be equal to 0.
+	 * \return Returns the result as ``remquo_result`` type.
+	 *
+	 * \detail This function is a more convenient version of the ``std::remquo`` function and simply forwards the call to that.
+	 */
+	template <class T>
+		requires requires { std::remquo(std::declval<T>(), std::declval<T>(), nullptr); }
+	[[nodiscard]]
+	SL_CONSTEXPR_MATH remquo_result<remquo_result_value_type<T>> remquo(T x, T y)
+	{
+		int quo{};
+		const auto remainder{ std::remquo(x, y, &quo) };
+		return { remainder, quo };
+	}
+
+	/**
 	 * \brief Result type for the modf operation.
 	 * \tparam T Used floating point type
 	 */
@@ -47,7 +85,7 @@ namespace sl::math
 	SL_CONSTEXPR_MATH modf_result<T> modf(T value)
 	{
 		T integral{};
-		T fraction = std::modf(value, &integral);
+		T fraction{ std::modf(value, &integral) };
 		return { integral, fraction };
 	}
 
