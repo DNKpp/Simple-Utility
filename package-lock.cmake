fe4f8beecfa27b9b7b0cfdb0a058b69b1efbaf7d
DNKpp <dnkpp2011@gmail.com>
2022-12-10 17:12:16 +0100

    deps: update catch2 to v3.2.1

--- package-lock.cmake
+++ package-lock.cmake
@@ -1,9 +1,6 @@
-# CPM Package Lock
-# This file should be committed to version control
-
 CPMDeclarePackage(
 	Catch2
-	GIT_TAG v3.1.0
+	GIT_TAG v3.2.1
 	GITHUB_REPOSITORY catchorg/Catch2
 	EXCLUDE_FROM_ALL YES
 )
