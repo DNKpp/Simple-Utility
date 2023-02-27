42cc2228cfcbcfbc1b06b326f1a162f2126c4abf
Dominic Koepke <koepke@aunovis.de>
2023-02-27 10:54:41 +0100

    fix: add get requirement to tuple_like concept

--- include/Simple-Utility/tuple_utility.hpp
+++ include/Simple-Utility/tuple_utility.hpp
@@ -21,22 +21,34 @@
 
 namespace sl::concepts::detail
 {
-	template <class TTuple>
-	consteval bool has_tuple_element()
+	template <std::size_t VIndex, class TTuple>
+	static constexpr bool check_tuple_index = []
 	{
-		return []<std::size_t... VIndices>([[maybe_unused]] std::index_sequence<VIndices...>)
-		{
-			return (requires { typename std::tuple_element_t<VIndices, TTuple>; } && ...);
-		}(std::make_index_sequence<std::tuple_size_v<TTuple>>{});
-	}
+		using std::get;
+
+		return requires{ typename std::tuple_element<VIndex, TTuple>::type; }
+			&& requires(TTuple t)
+			{
+				{ get<VIndex>(t) } -> std::common_with<std::tuple_element_t<VIndex, TTuple>>;
+				{ get<VIndex>(std::as_const(t)) } -> std::common_with<std::tuple_element_t<VIndex, TTuple>>;
+				{ get<VIndex>(std::move(t)) } -> std::common_with<std::tuple_element_t<VIndex, TTuple>>;
+				{ get<VIndex>(static_cast<const TTuple&&>(t)) } -> std::common_with<std::tuple_element_t<VIndex, TTuple>>;
+			};
+	}();
+
+	template <class TTuple>
+	static constexpr bool check_tuple_indices = []<std::size_t... VIndices>([[maybe_unused]] std::index_sequence<VIndices...>)
+	{
+		return (check_tuple_index<VIndices, TTuple> && ...);
+	}(std::make_index_sequence<std::tuple_size_v<TTuple>>{});
 }
 
 namespace sl::concepts
 {
 	/**
 	 * \brief Determines whether a type can be used as a tuple-like.
-	 * \details Requires the type to have a specialization of ```std::tuple_size`` and for each index
-	 * in the interval ``[0, N)``, the ``std::tuple_element`` trait must yield a valid type.
+	 * \details Requires the type-traits ``std::tuple_size`` to be defined for the given type and its member ``value`` denoting the the correct tuple size.
+	 * The ``std::tuple_element`` trait and the ``get`` function must to be defined for each index in the interval ``[0, N)``, where ``N`` is the tuple size of the given type.
 	 * \concept tuple_like
 	 * \ingroup GROUP_TUPLE_UTILITY GROUP_UTILITY_CONCEPTS
 	 */
@@ -46,7 +58,8 @@ namespace sl::concepts
 							typename std::tuple_size<TTuple>::type;
 							{ std::tuple_size_v<TTuple> } -> std::convertible_to<std::size_t>;
 						}
-						&& detail::has_tuple_element<TTuple>();
+						&& std::cmp_less_equal(0, std::tuple_size_v<TTuple>)
+						&& detail::check_tuple_indices<TTuple>;
 }
 
 namespace sl::detail
