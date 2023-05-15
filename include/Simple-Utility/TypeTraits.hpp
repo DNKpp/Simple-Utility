9841b070f1173d862d37d7df6aa0ecb99ed24d4c
DNKpp <dnkpp2011@gmail.com>
2023-05-16 01:08:21 +0200

    feat: add_type_volatile trait

--- include/Simple-Utility/TypeTraits.hpp
+++ include/Simple-Utility/TypeTraits.hpp
@@ -196,6 +196,98 @@ namespace sl
 	/**
 	 * \}
 	 */
+
+	/**
+	 * \defgroup GROUP_TYPE_TRAITS_ADD_TYPE_CONST add_type_volatile
+	 * \ingroup GROUP_TYPE_TRAITS
+	 * \brief This trait adds the ``const`` qualification to the actual type, instead of the top-level reference or pointer category.
+	 * \details Following some examples comparing the behaviour of ``std::add_volatile`` with ``sl::add_type_volatile``.
+	 * \see https://en.cppreference.com/w/cpp/types/add_cv
+	 * \snippet TypeTraits.cpp add_type_volatile
+	 * \{
+	 */
+
+	/**
+	 * \brief Primary template, adding const to value types.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	struct add_type_volatile
+	{
+		using type = volatile T;
+	};
+
+	/**
+	 * \brief Convenience alias, exposing the ``type`` member alias of the \ref sl::add_type_volatile "add_type_volatile" trait.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	using add_type_volatile_t = typename add_type_volatile<T>::type;
+
+	/**
+	 * \brief Specialization, unwrapping lvalue reference types.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	struct add_type_volatile<T&>
+	{
+		using type = add_type_volatile_t<T>&;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping rvalue reference types.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	struct add_type_volatile<T&&>
+	{
+		using type = add_type_volatile_t<T>&&;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping pointer types.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	struct add_type_volatile<T*>
+	{
+		using type = add_type_volatile_t<T>*;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping volatile pointer types.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	struct add_type_volatile<T* volatile>
+	{
+		using type = add_type_volatile_t<T>* volatile;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping const pointer types.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	struct add_type_volatile<T* const>
+	{
+		using type = add_type_volatile_t<T>* const;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping const volatile pointer types.
+	 * \tparam T Qualification to be applied to.
+	 */
+	template <class T>
+	struct add_type_volatile<T* const volatile>
+	{
+		using type = add_type_volatile_t<T>* const volatile;
+	};
+
+	/**
+	 * \}
+	 */
+
 	 * \}
 	 */
 }
