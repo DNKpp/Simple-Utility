8c6a973b54a1f0f81705893cc1d62350a29269b2
DNKpp <dnkpp2011@gmail.com>
2023-05-16 01:06:39 +0200

    feat: add remove_type_const

--- include/Simple-Utility/TypeTraits.hpp
+++ include/Simple-Utility/TypeTraits.hpp
@@ -106,6 +106,96 @@ namespace sl
 	 */
 
 	/**
+	 * \defgroup GROUP_TYPE_TRAITS_REMOVE_TYPE_CONST remove_type_const
+	 * \ingroup GROUP_TYPE_TRAITS
+	 * \brief This trait removes the ``const`` qualification of the actual type, instead of the top-level reference or pointer category.
+	 * \details Following some examples comparing the behaviour of ``std::remove_const`` with ``sl::remove_type_const``.
+	 * \see https://en.cppreference.com/w/cpp/types/remove_cv
+	 * \snippet TypeTraits.cpp remove_type_const
+	 * \{
+	 */
+
+	/**
+	 * \brief Primary template, removing const from value types.
+	 * \tparam T Qualification be removed from.
+	 * \detail This trait solves the usual struggle when removing const from a type with a reference category.
+	 */
+	template <class T>
+	struct remove_type_const
+	{
+		using type = std::remove_const_t<T>;
+	};
+
+	/**
+	 * \brief Convenience alias, exposing the ``type`` member alias of the \ref sl::remove_type_const "remove_type_const" trait.
+	 * \tparam T Qualification be removed from.
+	 */
+	template <class T>
+	using remove_type_const_t = typename remove_type_const<T>::type;
+
+	/**
+	 * \brief Specialization, unwrapping lvalue reference types.
+	 * \tparam T Qualification be removed from.
+	 */
+	template <class T>
+	struct remove_type_const<T&>
+	{
+		using type = remove_type_const_t<T>&;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping rvalue reference types.
+	 * \tparam T Qualification be removed from.
+	 */
+	template <class T>
+	struct remove_type_const<T&&>
+	{
+		using type = remove_type_const_t<T>&&;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping pointer types.
+	 * \tparam T Qualification to be removed from.
+	 */
+	template <class T>
+	struct remove_type_const<T*>
+	{
+		using type = remove_type_const_t<T>*;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping volatile pointer types.
+	 * \tparam T Qualification to be removed from.
+	 */
+	template <class T>
+	struct remove_type_const<T* volatile>
+	{
+		using type = remove_type_const_t<T>* volatile;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping const pointer types.
+	* \tparam T Qualification to be removed from.
+	 */
+	template <class T>
+	struct remove_type_const<T* const>
+	{
+		using type = remove_type_const_t<T>* const;
+	};
+
+	/**
+	 * \brief Specialization, unwrapping const volatile pointer types.
+	 * \tparam T Qualification to be removed from.
+	 */
+	template <class T>
+	struct remove_type_const<T* const volatile>
+	{
+		using type = remove_type_const_t<T>* const volatile;
+	};
+
+	/**
+	 * \}
+	 */
 	 * \}
 	 */
 }
