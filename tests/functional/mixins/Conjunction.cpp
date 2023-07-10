c3d1355da603528b0a08543285d8b715bc5f9f91
DNKpp <dnkpp2011@gmail.com>
2023-07-10 18:47:57 +0200

    refactor: move very similar cases into CompositionStrategies.cpp

--- tests/functional/mixins/Conjunction.cpp
+++ tests/functional/mixins/Conjunction.cpp
@@ -15,139 +15,6 @@ namespace sf = sl::functional;
 
 // ReSharper disable CppClangTidyBugproneUseAfterMove
 
-TEMPLATE_TEST_CASE_SIG(
-	"functional::ConjunctionStrategy invokes a single function with the input params.",
-	"[functional][functional::Conjunction]",
-	((bool fnResult), fnResult),
-	(true),
-	(false)
-)
-{
-	constexpr bool expected = fnResult;
-	constexpr sf::ConjunctionStrategy strategy{};
-
-	GenericOperation2Mock<bool, float, const std::string&> fun{};
-
-	SECTION("Functional provided as const lvalue ref.")
-	{
-		REQUIRE_CALL(fun, call_const_lvalue_ref(1337.f, "1337"))
-			.RETURN(fnResult);
-
-		const bool result = strategy(std::forward_as_tuple(std::as_const(fun)), 1337.f, "1337");
-
-		REQUIRE(expected == result);
-	}
-
-	SECTION("Functional provided as lvalue ref.")
-	{
-		REQUIRE_CALL(fun, call_lvalue_ref(1337.f, "1337"))
-			.RETURN(fnResult);
-
-		const bool result = strategy(std::forward_as_tuple(fun), 1337.f, "1337");
-
-		REQUIRE(expected == result);
-	}
-
-	SECTION("Functional provided as const rvalue ref.")
-	{
-		REQUIRE_CALL(fun, call_const_rvalue_ref(1337.f, "1337"))
-			.RETURN(fnResult);
-
-		const bool result = strategy(std::forward_as_tuple(std::move(std::as_const(fun))), 1337.f, "1337");
-
-		REQUIRE(expected == result);
-	}
-
-	SECTION("Functional provided as rvalue ref.")
-	{
-		REQUIRE_CALL(fun, call_rvalue_ref(1337.f, "1337"))
-			.RETURN(fnResult);
-
-		const bool result = strategy(std::forward_as_tuple(std::move(fun)), 1337.f, "1337");
-
-		REQUIRE(expected == result);
-	}
-}
-
-TEMPLATE_TEST_CASE_SIG(
-	"functional::ConjunctionStrategy with two functions invokes all functions with the given arguments.",
-	"[functional][functional::Conjunction]",
-	((bool firstResult, bool secondResult), firstResult, secondResult),
-	(true, true),
-	(true, false) /*,
-	let this test via a separate test
-	(false, true),
-	(false, false)*/
-)
-{
-	constexpr bool expected = firstResult && secondResult;
-	constexpr sf::ConjunctionStrategy strategy{};
-
-	GenericOperation1Mock<bool, int> first{};
-	GenericOperation1Mock<bool, int> second{};
-
-	trompeloeil::sequence seq{};
-
-	SECTION("Functionals provided as const lvalue ref.")
-	{
-		REQUIRE_CALL(first, call_const_lvalue_ref(42))
-		   .RETURN(firstResult)
-		   .IN_SEQUENCE(seq);
-
-		REQUIRE_CALL(second, call_const_lvalue_ref(42))
-			.RETURN(secondResult)
-			.IN_SEQUENCE(seq);
-
-		const bool result = strategy(std::forward_as_tuple(std::as_const(first), std::as_const(second)), 42);
-
-		REQUIRE(expected == result);
-	}
-
-	SECTION("Functionals provided as lvalue ref.")
-	{
-		REQUIRE_CALL(first, call_lvalue_ref(42))
-			.RETURN(firstResult)
-			.IN_SEQUENCE(seq);
-
-		REQUIRE_CALL(second, call_lvalue_ref(42))
-			.RETURN(secondResult)
-			.IN_SEQUENCE(seq);
-
-		const bool result = strategy(std::forward_as_tuple(first, second), 42);
-
-		REQUIRE(expected == result);
-	}
-
-	SECTION("Functionals provided as const rvalue ref.")
-	{
-		REQUIRE_CALL(first, call_const_rvalue_ref(42))
-			.RETURN(firstResult)
-			.IN_SEQUENCE(seq);
-
-		REQUIRE_CALL(second, call_const_rvalue_ref(42))
-			.RETURN(secondResult)
-			.IN_SEQUENCE(seq);
-
-		const bool result = strategy(std::forward_as_tuple(std::move(std::as_const(first)), std::move(std::as_const(second))), 42);
-
-		REQUIRE(expected == result);
-	}
-
-	SECTION("Functionals provided as rvalue ref.")
-	{
-		REQUIRE_CALL(first, call_rvalue_ref(42))
-			.RETURN(firstResult)
-			.IN_SEQUENCE(seq);
-
-		REQUIRE_CALL(second, call_rvalue_ref(42))
-			.RETURN(secondResult)
-			.IN_SEQUENCE(seq);
-
-		const bool result = strategy(std::forward_as_tuple(std::move(first), std::move(second)), 42);
-
-		REQUIRE(expected == result);
-	}
-}
 
 TEST_CASE(
 	"functional::ConjunctionStrategy with multiple functions invokes each function until false was returned.",
@@ -192,30 +59,11 @@ TEST_CASE(
 	}
 }
 
-TEMPLATE_TEST_CASE_SIG(
-	"functional::ConjunctionStrategy propagates noexcept specification.",
-	"[functional][functional::Conjunction]",
-	((bool expected, class... Funs), expected, Funs...),
-	(true, NoThrowInvokable<true>),
-	(true, NoThrowInvokable<true>, NoThrowInvokable<true>),
-	(true, NoThrowInvokable<true>, NoThrowInvokable<true>, NoThrowInvokable<true>),
-	(false, NoThrowInvokable<false>),
-	(false, NoThrowInvokable<false>, NoThrowInvokable<true>),
-	(false, NoThrowInvokable<true>, NoThrowInvokable<false>),
-	(false, NoThrowInvokable<false>, NoThrowInvokable<false>),
-	(false, NoThrowInvokable<false>, NoThrowInvokable<true>, NoThrowInvokable<true>),
-	(false, NoThrowInvokable<true>, NoThrowInvokable<false>, NoThrowInvokable<true>),
-	(false, NoThrowInvokable<true>, NoThrowInvokable<true>, NoThrowInvokable<false>)
-)
-{
-	STATIC_REQUIRE(expected == std::is_nothrow_invocable_v<sf::ConjunctionStrategy, std::tuple<Funs...>, int>);
-}
-
 template <class Fn>
 using Closure = sf::BasicClosure<Fn, sf::BasicInvokePolicy, sf::ConjunctionOperator>;
 
 TEST_CASE(
-	"functional::ConjunctionOperator enables enables && composing for functional::BasicClosure.",
+	"functional::ConjunctionOperator enables && composing for functional::BasicClosure.",
 	"[functional][functional::Conjunction]"
 )
 {
