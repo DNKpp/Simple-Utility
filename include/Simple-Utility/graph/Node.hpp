28ca5ec7bee2b22efb307a7d89d8c78c71cc5c5b
DNKpp <dnkpp2011@gmail.com>
2023-08-20 22:34:08 +0200

    feat: graph::node::vertex customization point

---
+++ include/Simple-Utility/graph/Node.hpp
@@ -0,0 +1,41 @@
+//          Copyright Dominic Koepke 2019 - 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef SIMPLE_UTILITY_GRAPH_NODE_HPP
+#define SIMPLE_UTILITY_GRAPH_NODE_HPP
+
+#include "Simple-Utility/concepts/stl_extensions.hpp"
+#include "Simple-Utility/graph/Common.hpp"
+
+namespace sl::graph::node::detail
+{
+	struct vertex_fn
+	{
+		constexpr auto& operator ()(const auto& node) const noexcept
+			requires concepts::vertex<std::remove_cvref_t<decltype(node.vertex)>>
+		{
+			return node.vertex;
+		}
+
+		constexpr decltype(auto) operator ()(const auto& node) const noexcept
+			requires concepts::vertex<std::remove_cvref_t<decltype(node.vertex())>>
+		{
+			return node.vertex();
+		}
+
+		constexpr decltype(auto) operator ()(const auto& node) const noexcept
+			requires concepts::vertex<std::remove_cvref_t<decltype(vertex(node))>>
+		{
+			return vertex(node);
+		}
+	};
+}
+
+namespace sl::graph::node
+{
+	inline constexpr detail::vertex_fn vertex{};
+}
+
+#endif
---
+++ tests/graph/Node.cpp
@@ -0,0 +1,61 @@
+//          Copyright Dominic Koepke 2019 - 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "Simple-Utility/graph/Node.hpp"
+
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_adapters.hpp>
+#include <catch2/generators/catch_generators_random.hpp>
+
+#include "Defines.hpp"
+
+namespace
+{
+	struct member_vertex
+	{
+		int vertex;
+	};
+
+	struct member_fun_vertex
+	{
+		MAKE_CONST_MOCK0(vertex, int());
+	};
+
+	struct free_fun_vertex
+	{
+		MAKE_CONST_MOCK0(my_vertex, int());
+
+		friend int vertex(const free_fun_vertex& v)
+		{
+			return v.my_vertex();
+		}
+	};
+}
+
+TEST_CASE("graph::node::vertex serves as a customization point accessing the node vertex.", "[graph][graph::node]")
+{
+	const int expected = GENERATE(take(5, random(0, std::numeric_limits<int>::max())));
+
+	SECTION("Access via the vertex member.")
+	{
+		REQUIRE(expected == sg::node::vertex(member_vertex{expected}));
+	}
+
+	SECTION("Access via the vertex member function.")
+	{
+		member_fun_vertex mock{};
+		REQUIRE_CALL(mock, vertex())
+			.RETURN(expected);
+		REQUIRE(expected == sg::node::vertex(std::as_const(mock)));
+	}
+
+	SECTION("Access via the vertex free function.")
+	{
+		free_fun_vertex mock{};
+		REQUIRE_CALL(mock, my_vertex())
+			.RETURN(expected);
+		REQUIRE(expected == sg::node::vertex(std::as_const(mock)));
+	}
+}
