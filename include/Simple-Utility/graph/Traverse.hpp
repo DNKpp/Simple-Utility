e706c35c48d6ceeb410ff7b15a1f891f6243892b
DNKpp <dnkpp2011@gmail.com>
2023-08-21 14:54:35 +0200

    feat: graph::detail::BasicState

---
+++ include/Simple-Utility/graph/Traverse.hpp
@@ -0,0 +1,70 @@
+//          Copyright Dominic Koepke 2019 - 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef SIMPLE_UTILITY_GRAPH_TRAVERSE_HPP
+#define SIMPLE_UTILITY_GRAPH_TRAVERSE_HPP
+
+#pragma once
+
+#include "Simple-Utility/Config.hpp"
+#include "Simple-Utility/concepts/stl_extensions.hpp"
+#include "Simple-Utility/functional/Tuple.hpp"
+#include "Simple-Utility/graph/Common.hpp"
+#include "Simple-Utility/graph/Node.hpp"
+#include "Simple-Utility/graph/Queue.hpp"
+#include "Simple-Utility/graph/Tracker.hpp"
+
+#include <concepts>
+#include <ranges>
+#include <type_traits>
+
+namespace sl::graph::detail
+{
+	template <concepts::node Node, concepts::queue_for<Node> QueuingStrategy>
+	class BasicState
+	{
+	public:
+		using node_type = Node;
+		using queue_type = QueuingStrategy;
+
+		[[nodiscard]]
+		constexpr explicit BasicState(const node_type& origin)
+			: BasicState{std::move(origin), {}}
+		{
+		}
+
+		[[nodiscard]]
+		constexpr explicit BasicState(const node_type& origin, queue_type queue)
+			: m_Queue{std::move(queue)}
+		{
+			queue::insert(m_Queue, std::views::single(origin));
+		}
+
+		[[nodiscard]]
+		constexpr const queue_type& queue() const noexcept
+		{
+			return m_Queue;
+		}
+
+		template <class Neighbors>
+			requires std::convertible_to<std::ranges::range_value_t<Neighbors>, node_type>
+		[[nodiscard]]
+		constexpr std::optional<node_type> next(Neighbors&& neighbors)
+		{
+			queue::insert(m_Queue, std::forward<Neighbors>(neighbors));
+			if (!queue::empty(m_Queue))
+			{
+				return queue::next(m_Queue);
+			}
+
+			return std::nullopt;
+		}
+
+	private:
+		QueuingStrategy m_Queue{};
+	};
+}
+
+#endif
---
+++ tests/graph/Traverse.cpp
@@ -0,0 +1,126 @@
+//          Copyright Dominic Koepke 2019 - 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_adapters.hpp>
+#include <catch2/generators/catch_generators_random.hpp>
+
+#include "Defines.hpp"
+
+#include "Simple-Utility/graph/Traverse.hpp"
+
+#include <numeric>
+
+// ReSharper disable CppDeclaratorNeverUsed
+// ReSharper disable CppTypeAliasNeverUsed
+// ReSharper disable CppClangTidyClangDiagnosticUnneededMemberFunction
+
+namespace
+{
+	struct VertexMemberNode
+	{
+		using vertex_type = int;
+
+		int vertex{};
+
+		friend bool operator==(const VertexMemberNode&, const VertexMemberNode&) = default;
+	};
+
+	struct DefaultConstructibleQueue
+	{
+		std::vector<VertexMemberNode> queue{};
+
+		[[nodiscard]]
+		bool empty() const noexcept
+		{
+			return queue.empty();
+		}
+
+		void insert(const auto& elements)
+		{
+			queue.insert(std::end(queue), std::ranges::begin(elements), std::ranges::end(elements));
+		}
+
+		[[nodiscard]]
+		VertexMemberNode next()
+		{
+			const VertexMemberNode front = queue.front();
+			queue.erase(std::begin(queue));
+			return front;
+		}
+	};
+}
+
+using DefaultNode = VertexMemberNode;
+using DefaultQueue = QueueMock<DefaultNode>;
+using DefaultState = sg::detail::BasicState<DefaultNode, DefaultQueue>;
+
+TEST_CASE("BasicState is constructible from origin node and queue argument.", "[graph][graph::traverse][graph::detail]")
+{
+	using trompeloeil::_;
+
+	const int originVertex = GENERATE(take(5, random(std::numeric_limits<int>::min(), std::numeric_limits<int>::max())));
+
+	DefaultQueue queueMock{};
+	REQUIRE_CALL(queueMock, do_insert(_))
+		.WITH(std::ssize(_1) == 1)
+		.WITH(sg::node::vertex(_1[0]) == originVertex);
+
+	const DefaultState state{{originVertex}, std::move(queueMock)};
+}
+
+TEST_CASE("BasicState is constructible from origin node.", "[graph][graph::traverse][graph::detail]")
+{
+	const int originVertex = GENERATE(take(5, random(std::numeric_limits<int>::min(), std::numeric_limits<int>::max())));
+
+	const sg::detail::BasicState<VertexMemberNode, DefaultConstructibleQueue> state{{originVertex}};
+
+	REQUIRE(originVertex == sg::node::vertex(state.queue().queue.front()));
+}
+
+TEST_CASE("BasicState::next accepts current neighbors and returns the next node, if present.", "[graph][graph::traverse][graph::detail]")
+{
+	using trompeloeil::_;
+
+	DefaultQueue queueMock{};
+	REQUIRE_CALL(queueMock, do_insert(_))
+		.WITH(std::ranges::equal(std::array{DefaultNode{42}}, _1));
+	DefaultState state{{42}, std::move(queueMock)};
+
+	SECTION("Providing neighbor infos on next call.")
+	{
+		std::vector<DefaultNode> neighbors{{44}, {45}, {46}};
+		auto& queueMember = const_cast<DefaultQueue&>(state.queue());
+
+		trompeloeil::sequence seq{};
+		REQUIRE_CALL(queueMember, do_insert(_))
+			.WITH(std::ranges::is_permutation(neighbors, _1))
+			.IN_SEQUENCE(seq);
+
+		REQUIRE_CALL(queueMember, empty())
+			.RETURN(false)
+			.IN_SEQUENCE(seq);
+
+		REQUIRE_CALL(queueMember, next())
+			.RETURN(DefaultNode{42})
+			.IN_SEQUENCE(seq);
+
+		REQUIRE(DefaultNode{42} == state.next(neighbors));
+
+		SECTION("And when the internal queue depletes.")
+		{
+			REQUIRE_CALL(queueMember, do_insert(_))
+				.WITH(std::ranges::empty(_1))
+				.IN_SEQUENCE(seq);
+
+			REQUIRE_CALL(queueMember, empty())
+				.RETURN(true)
+				.IN_SEQUENCE(seq);
+
+			REQUIRE(std::nullopt == state.next(std::array<DefaultNode, 0>{}));
+		}
+	}
+}
