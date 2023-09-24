53fe14514bc5a52853ae540579afe89a3953f121
DNKpp <dnkpp2011@gmail.com>
2023-09-24 20:51:52 +0200

    feat: SL_UTILITY_HAS_RANGES_VIEWS macro

--- include/Simple-Utility/Config.hpp
+++ include/Simple-Utility/Config.hpp
@@ -35,4 +35,9 @@
 	#define SL_UTILITY_HAS_STD_FORMAT
 #endif
 
+#if not (defined(__clang__) && __clang_major__ < 16) \
+	|| (defined(__GNUG__) && __GNUG__ < 12)
+	#define SL_UTILITY_HAS_RANGES_VIEWS
+#endif
+
 #endif
