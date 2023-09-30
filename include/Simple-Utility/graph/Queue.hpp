c27b0a78a74dd0842d1a75abdd5a026d5e36cef1
DNKpp <dnkpp2011@gmail.com>
2023-09-30 19:13:09 +0200

    refactor: graph::concepts::queue_for test range

--- include/Simple-Utility/graph/Queue.hpp
+++ include/Simple-Utility/graph/Queue.hpp
@@ -157,63 +157,38 @@ namespace sl::graph::queue
 	inline constexpr detail::next_fn next{};
 }
 
-#if __clang__ && __clang_major__ < 16
-
-#include <forward_list>
-
 namespace sl::graph::queue::detail
 {
 	template <class T>
-	using dummy_input_range = std::forward_list<T>;
-}
-
-#else
-
-namespace sl::graph::queue::detail
-{
-	/* This defines a dummy input_iterator, which we need to declare a input_range, which is then used
-	 * to validate the graph::insert conformity in the queue_for concept.
-	 */
-	template <class T>
-	struct input_range_maker
+	struct dummy_input_range
 	{
-		struct input_iterator
+		// ReSharper disable CppFunctionIsNotImplemented
+		struct iterator
 		{
 			using iterator_concept = std::input_iterator_tag;
 			using element_type = T;
 			using difference_type = std::ptrdiff_t;
 
-			T* dummy{};
-
-			// ReSharper disable once CppFunctionIsNotImplemented
-			T& operator *() const { return *dummy; }
-
-			// ReSharper disable once CppFunctionIsNotImplemented
-			input_iterator& operator ++();
-			// ReSharper disable once CppFunctionIsNotImplemented
+			T& operator *() const;
+			iterator& operator ++();
 			void operator ++(int);
-			bool operator==(const input_iterator&) const = default;
+			bool operator==(const iterator&) const;
 		};
 
-		static_assert(std::input_iterator<input_iterator>);
-		static_assert(!std::forward_iterator<input_iterator>);
-
-		using type = std::ranges::subrange<input_iterator>;
+		iterator begin();
+		iterator end();
+		// ReSharper restore CppFunctionIsNotImplemented
 	};
 
-	template <class T>
-	using dummy_input_range = typename input_range_maker<T>::type;
+	static_assert(std::ranges::input_range<dummy_input_range<int>>);
 }
-
-#endif
 
 namespace sl::graph::concepts
 {
 	template <class T, class Node>
 	concept queue_for = sl::concepts::unqualified<T>
 						&& basic_node<Node>
-						// ReSharper disable once CppRedundantTemplateKeyword
-						&& requires(T& queue, queue::detail::template dummy_input_range<Node> inputRange)
+						&& requires(T& queue, queue::detail::dummy_input_range<Node> inputRange)
 						{
 							{ queue::empty(std::as_const(queue)) } -> std::convertible_to<bool>;
 							queue::insert(queue, inputRange);
